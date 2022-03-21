8af48c10aaf1c1b65f0a8df0711f40ee52d04c72
Abel Gaming <lcpdpolice4939@gmail.com>
2022-03-20 20:37:03 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,7 +2,7 @@ Config = {}
 
 -- Discord
 Config.EnableDiscordJoinNotifications = true
-Config.JoinNotificationWebhook = 'https://discordapp.com/api/webhooks/914011011764350976/DXBvMGIymocDySha6Y98JV3Q7IIBasznN_HHlNrIdBnlyr48p1oygN2swRytz3nU6mOt'
+Config.JoinNotificationWebhook = 'WEBHOOK URL HERE'
 
 Config.EnableDiscordLeaveNotifications = true
-Config.LeaveNotificationWebhook = 'https://discordapp.com/api/webhooks/914011011764350976/DXBvMGIymocDySha6Y98JV3Q7IIBasznN_HHlNrIdBnlyr48p1oygN2swRytz3nU6mOt'
+Config.LeaveNotificationWebhook = 'WEBHOOK URL HERE'
